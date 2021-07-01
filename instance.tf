f87f4cb8b4ba4b8eb9d267e46b666209e2157af1
Farrukh Sadykov <farrukhsadykov@gmail.com>
2021-06-30 23:55:55 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -88,7 +88,7 @@ resource "aws_instance" "awx" {
   key_name                    = aws_key_pair.ansible.key_name
   associate_public_ip_address = "true"
   subnet_id = module.vpc.public_subnets[0]
-  security_groups   = [aws_security_group.used-for-ansible-tower.id]
+  vpc_security_group_ids   = [aws_security_group.used-for-ansible-tower.id]
 
   provisioner "file" {
     source      = "awx"
