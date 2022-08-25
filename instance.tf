76653e21c350a30f52a76bd08bce0f8a604a86d3
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-08-25 18:36:24 -0500

    removed data

--- instance.tf
+++ instance.tf
@@ -8,18 +8,18 @@ resource "aws_key_pair" "ansible" {
 }
 
 
-data "aws_ami" "centos" {
-  most_recent = true
-  owners      = ["679593333241"]
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-  filter {
-    name   = "name"
-    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
-  }
-}
+# data "aws_ami" "centos" {
+#   most_recent = true
+#   owners      = ["679593333241"]
+#   filter {
+#     name   = "state"
+#     values = ["available"]
+#   }
+#   filter {
+#     name   = "name"
+#     values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+#   }
+# }
 
 resource "aws_security_group" "used-for-ansible-tower" {
   name        = "used-for-ansible-tower"
