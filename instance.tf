edc32882ae7147043fc1eb4395066e06c9dc00f1
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-08-25 18:19:55 -0500

    ami updated

--- instance.tf
+++ instance.tf
@@ -81,7 +81,7 @@ output "region" {
 
 resource "aws_instance" "awx" {
   instance_type               = var.awx_config["instance_type"]
-  ami                         = data.aws_ami.centos.id
+  ami                         = "ami-065f579dda2ccf461"
   key_name                    = aws_key_pair.ansible.key_name
   associate_public_ip_address = "true"
   subnet_id                   = module.vpc.public_subnets[0]
